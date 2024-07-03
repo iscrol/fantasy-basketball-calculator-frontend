e5e1d3e2c700ae72fd53a772d33447e3a6b6d9b8
Ian Scrol <imscrol@gmail.com>
2024-07-03 16:52:15 -0400

    fixing dependency

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import './App.css';
 import Title from "./components/title/Title";
 import HelpButton from './components/help-button/HelpButton';
@@ -11,32 +11,23 @@ function App() {
     // State to manage which statistics are active
     const [activeStats, setActiveStats] = useState(["PTS", "REB", "AST", "STL", "BLK", "3PM", "FG%", "FT%", "TO"]);
 
-    // Function to fetch player data based on active statistics
-    const fetchPlayers = async () => {
-      try {
-          const statsQuery = activeStats.join('&stats=');
-          const apiUrl = import.meta.env.VITE_API_BASE_URL;
-          const response = await fetch(`${apiUrl}?stats=${statsQuery}`);
-          const data = await response.json();
-          console.log(data);  // Log data here to inspect it
-          setPlayers(data);
-      } catch (error) {
-          console.error('Failed to fetch players:', error);
-      }
-  };
-  
+    // Function to fetch player data based on active statistics, wrapped with useCallback
+    const fetchPlayers = useCallback(async () => {
+        try {
+            const statsQuery = activeStats.join('&stats=');
+            const apiUrl = import.meta.env.VITE_API_BASE_URL; // Make sure this environment variable is set in Netlify
+            const response = await fetch(`${apiUrl}?stats=${statsQuery}`);
+            const data = await response.json();
+            setPlayers(data);
+        } catch (error) {
+            console.error('Failed to fetch players:', error);
+        }
+    }, [activeStats]);  // Include activeStats in the dependency array of useCallback
 
     // Effect to fetch players when activeStats changes
     useEffect(() => {
         fetchPlayers();
-    }, [activeStats]);
-
-    // Function to handle toggling statistics
-    const handleToggleStat = (stat) => {
-        setActiveStats(prev => 
-            prev.includes(stat) ? prev.filter(s => s !== stat) : [...prev, stat]
-        );
-    };
+    }, [fetchPlayers]);  // Include fetchPlayers in the dependency array of useEffect
 
     return (
         <div>
@@ -44,7 +35,7 @@ function App() {
                 <HelpButton />
             </header>
             <Title />
-            <StatisticalButtons activeStats={activeStats} onToggleStat={handleToggleStat} />
+            <StatisticalButtons activeStats={activeStats} onToggleStat={setActiveStats} />
             <PlayerTable players={players} />
         </div>
     );
